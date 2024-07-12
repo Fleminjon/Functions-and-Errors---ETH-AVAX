e37b89b239906b38b97c963e495dc12eb334b666
jurich <104262348+Fleminjon@users.noreply.github.com>
2024-07-12 22:27:02 +0800

    Update Error.sol

--- Error.sol
+++ Error.sol
@@ -1,25 +1,30 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ProjectMod1 {
+contract ErrorHandling {
     uint256 public value;
 
-    // Sets the value to a given _value
-    function setValue(uint256 _value) public {
+    // Function to set a value, require statement ensures value is greater than 0
+    function ValueSetter(uint256 _value) public {
+        require(_value > 0, "Value must be greater than 0");
         value = _value;
     }
 
-    // Checks if the inputted _value is within the range and if it's exactly 5
-    function checkValue(uint256 _value) public pure {
-        require(_value >= 5 && _value <= 20, "The input should be a value between 5 and 20");
-
-        if (_value != 5) {
-            revert("The input value must be 5");
-        }
+    // Function uses require statemnt to check if value is less than a specified threshold
+    function ValueChecker(uint256 _threshold) public view {
+        require(value < _threshold, "Value is not less than the given value");
     }
 
-    // Asserts that the current value is greater than 1
-    function asserting() public view {
-        assert(value > 1);
+    // Function to assert that the current value is not zero
+    function Assert() public view {
+        assert(value != 0);
+    }
+
+    // Function to demonstrate an error using revert
+    function simulateError() public pure {
+        uint256 a = 1;
+        uint256 b = 0;
+        uint256 c = a / b; // This will cause a divide by zero error
+        revert("Simulated internal error");
     }
 }
