52c3726b24c5240976c86ccc548e1b55c388a731
jurich <104262348+Fleminjon@users.noreply.github.com>
2024-07-16 23:09:07 +0800

    Update Error.sol

--- Error.sol
+++ Error.sol
@@ -4,31 +4,40 @@ pragma solidity ^0.8.0;
 contract HandleMyError {
     string public message = "REal or FakE";
     uint256 public value;
+    address public owner;
 
-    
-    function REquiring(uint _value) public {
-        require(_value > 18, "Value illegal, must be greater than 18");
+    constructor() {
+        owner = msg.sender;
+    }
+    function setEven(uint _value) public {
+        require(_value % 2 == 0, "Value must be an even number");    // it requires that the value is an even number
         value = _value;
+        message = "Even value set successfully";
     }
 
-
-     function Reverting(uint _value) public {
-        if (_value <= 18) {
-            revert("Value reverted because illegal, must be greater than 18");
+    function setNotByFive(uint _value) public {
+        if (_value % 5 == 0) {
+            revert("Value must not be a multiple of 5");    // it reverts if the value is a multiple of 5
         }
-        message = "Reverted";
+        value = _value;
+        message = "Value set successfully and is not a multiple of 5";
     }
-    function Checker(uint _value) public {
-        assert(_value != 17);
-        message = "Value is legal, Assertion needs are met";
-    }
-    function Adder(uint256 amount) public {
-        require(amount > 18, "Value illegal, must be greater than 18");
-        value += amount;
+    
 
+    function Remove(uint256 amount) public {
+        require(amount <= value, "Amount must be less than or equal to current value");
+        value -= amount;    // subtracts an amount from the value with a require check
+        message = "Amount subtracted successfully";
     }
-
-    function CallMessage() public {
-        message = "This is an additional function tests my Callmessage function"; 
+    function PlusAssert(uint _value) public {
+        uint threshold = 100;
+        uint newValue = value + _value;
+        assert(newValue > threshold);    // Asserts that the sum of value and input is greater than the threshold
+        value = newValue;
+        message = "Value added and Assert passed";
+    }
+    // Function to change the message
+    function changeMessage(string memory newMessage) public {
+        message = newMessage;
     }
 }
